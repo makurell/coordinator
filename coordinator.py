03f0083311e70aa605797cd8f77aa3c0476402a8
makurell <makureoll@gmail.com>
2018-09-04 04:16:52 +0100

    updt vis

--- coordinator.py
+++ coordinator.py
@@ -7,10 +7,16 @@ class Slot:
     def __init__(self,name="",off=0.0,length=0.0):
         colorama.init()
 
+        self.parent:Slot=None
         self.timeline:List[Slot]=[]
         self.name = name
         self.off = off # (relative) start time in seconds
         self.length = length # length in seconds
+
+    def add(self, *slots):
+        for slot in slots:
+            slot.parent=self
+            self.timeline.append(slot)
 
     def get_span_length(self):
         """
@@ -21,6 +27,14 @@ class Slot:
             return (self.timeline[-1].off + self.timeline[-1].length) - self.timeline[0].off
         else:
             return self.length
+
+    def abs_off(self):
+        par = self.parent
+        curoff = self.off
+        while par is not None:
+            curoff+=par.off
+            par=par.parent
+        return curoff
 
     def sort(self):
         self.timeline.sort(key=lambda x: x.off)
@@ -33,23 +47,23 @@ class Slot:
         buf=""
         prev_index=0
         for child in self.timeline:
-            buf+=' '*math.floor((child.off-prev_index))
+            buf+=' '*math.floor((child.abs_off()-prev_index))
 
             if child.length>2:
                 maxl=(math.floor(child.length)-2)
-                name=(child.name+' ')[:maxl]
-                buf+='|'+name+('x'*(maxl-len(name)))+'|'
+                name=(child.name+'')[:maxl]
+                buf+='|'+name+('-'*(maxl-len(name)))+'|'
             else:
                 buf+='|'*math.floor(child.length)
 
             ch_vis=child.visualisation()
             if len(ch_vis.strip())>0:
                 ret.append(ch_vis)
-            prev_index=child.off+child.length
+            prev_index=child.abs_off()+child.length
         ret.append(buf)
 
         return '\n'.join(ret)
 
 class Coordinator(Slot):
     def __init__(self):
-        super().__init__("root")
+        super().__init__(name="root")
--- tests.py
+++ tests.py
@@ -4,11 +4,14 @@ import time
 from coordinator import Coordinator, Slot
 
 c=Coordinator()
-a=Slot("A",0,70)
-a.timeline.extend([Slot("Aa",1,50),
-                   Slot("Ab",56,10)])
-c.timeline.extend([a,
-                   Slot("B",71,2),
-                   Slot("Ccccccccccc",75,3),
-                   Slot("D",79,10)])
+a=Slot("A", 0, 70)
+aa=Slot("AA", 2, 50)
+aa.add(Slot("AAa", 8, 10),
+       Slot("AAb", 20, 6))
+a.add(aa,
+      Slot("Ab", 56, 10))
+c.add(a,
+      Slot("B", 71, 2),
+      Slot("Ccccccccccc", 75, 3),
+      Slot("D", 79, 10))
 print(c.visualisation())
